f25ab8c2199c123676e9bff9cd4bcd5f76675035
MrChomp <162981668+MrChompDev@users.noreply.github.com>
2026-09-02 16:52:39 +1000

    Delete src directory

--- src/pages/about.ts
+++
@@ -1,8 +0,0 @@
-import '../style.css';
-import { initThree } from '../three/init';
-import { workshopScene } from '../three/workshop-scene';
-
-initThree('hero-3d-canvas', {
-  cameraZ: 5,
-  content: (s) => workshopScene(s),
-});
--- src/pages/home.ts
+++
@@ -1,8 +0,0 @@
-import '../style.css';
-import { initThree } from '../three/init';
-import { forgeScene } from '../three/forge-scene';
-
-initThree('hero-3d-canvas', {
-  cameraZ: 4.5,
-  content: (s) => forgeScene(s),
-});
